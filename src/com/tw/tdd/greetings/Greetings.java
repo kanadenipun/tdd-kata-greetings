a734c3935d5c55921d3dd915d5c651ed817b7d14
kanadenipun <nipunk@thoughtworks.com>
2017-04-03 10:16:59 +0530

    req1 : first failing test

--- src/com/tw/tdd/greetings/Greetings.java
+++ src/com/tw/tdd/greetings/Greetings.java
@@ -2,4 +2,7 @@ package com.tw.tdd.greetings;
 
 public class Greetings {
 
+    public String greet(String name) {
+        return null;
+    }
 }
--- test/com/tw/tdd/greetings/GreetingsTest.java
+++ test/com/tw/tdd/greetings/GreetingsTest.java
@@ -1,5 +1,16 @@
 package com.tw.tdd.greetings;
 
-public class GreetingsTest {
+import static org.junit.Assert.assertEquals;
 
+public class GreetingsTest {
+    @org.junit.Test
+    public void shouldGreetBob() throws Exception {
+        //given
+            Greetings greetings = new Greetings();
+            String expectedResult = "Hello Bob";
+        //when
+            String actualResult = greetings.greet("Bob");
+        //then
+            assertEquals(expectedResult, actualResult);
+    }
 }
