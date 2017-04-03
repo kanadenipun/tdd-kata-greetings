c49568a407ef3937e01f99e73a6d4a59398e6093
kanadenipun <nipunk@thoughtworks.com>
2017-04-03 10:22:59 +0530

    req1: second failing test added

--- test/com/tw/tdd/greetings/GreetingsTest.java
+++ test/com/tw/tdd/greetings/GreetingsTest.java
@@ -1,16 +1,29 @@
 package com.tw.tdd.greetings;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class GreetingsTest {
     @org.junit.Test
     public void shouldGreetBob() throws Exception {
         //given
-            Greetings greetings = new Greetings();
-            String expectedResult = "Hello Bob";
+        Greetings greetings = new Greetings();
+        String expectedResult = "Hello Bob";
         //when
-            String actualResult = greetings.greet("Bob");
+        String actualResult = greetings.greet("Bob");
         //then
-            assertEquals(expectedResult, actualResult);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void shouldGreetJohn() throws Exception {
+        //given
+        Greetings greetings = new Greetings();
+        String expectedResult = "Hello John";
+        //when
+        String actualResult = greetings.greet("John");
+        //then
+        assertEquals(expectedResult, actualResult);
     }
 }
